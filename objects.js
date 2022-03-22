cfdf4dba68be70a80e9209e15df38efbadd77773
Madeline Prickette <madelineprickette@gmail.com>
2022-03-22 14:54:59 -0500

    began coding with video

---
+++ objects.js
@@ -0,0 +1,11 @@
+console.log('working');
+
+let card0 = {
+//This is an object literal ^^
+    suit: 'Hearts',
+    number: 2
+   //This will set the properties of the object 
+   //Assign values with a colon :
+   //Use comma between each one ,
+   //Chrome will list objects in alphabetical order
+}//end card0
